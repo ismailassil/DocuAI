39938c99f2bdf5e990f0cf80e851b909dd605001
Ismail Assil <ismailassil00@gmail.com>
2025-09-15 10:39:38 +0100

    clean

--- backend/src/ai/ai.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AIService } from './ai.service';
-import { AIController } from './ai.controller';
-
-@Module({
-  providers: [AIService],
-  controllers: [AIController],
-})
-export class AIModule {}
--- backend/src/ai/entities/question.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class QuestionDTO {
-  @IsString()
-  @IsNotEmpty()
-  question: string;
-}
